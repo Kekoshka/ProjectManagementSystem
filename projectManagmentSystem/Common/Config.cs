1560caae85cf9c24069004497b125ee888111d8a
Alexandr Isakov <ks-22-1-8@permaviat.ru>
2025-04-01 09:24:28 +0500

    Добавьте файлы проекта.

---
+++ projectManagmentSystem/Common/Config.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projectManagmentSystem.Common
+{
+    public class Config
+    {
+        public readonly string ConnectionString = "Server=;Database=;Trusted_Connection=true;Encrypt=false";
+    }
+}
---
+++ projectManagmentSystem/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace projectManagmentSystem
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+
+    {
+        public static MainWindow Instance;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Instance = this;
+            OpenPage(new Pages.Authorization());
+        }
+        public static void OpenPage(Page page)
+        {
+            MainWindow.Instance.frame.Navigate(page);
+        }
+    }
+}
